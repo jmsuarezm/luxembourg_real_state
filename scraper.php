2c40822bf0a463818d37334ddf437300eec492c6
Juan Manuel Suarez <yo.juanma@gmail.com>
2015-02-27 16:19:10 +0100

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -24,3 +24,76 @@
 // is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
 // has at least a table called data.
 ?>
+
+<?php
+require 'scraperwiki.php';
+require 'scraperwiki/simple_html_dom.php';
+ini_set('max_execution_time', 600);
+setlocale(LC_ALL, 'fr_FR.UTF8');
+
+//the firt time, we set manually the first page
+$value = "/recherche/resultats/tr/by/ig/h,f";
+//if $value is a valid page
+while ($value != "") {
+    $htmlCentre = scraperWiki::scrape("http://www.athome.lu" . $value);
+    $domCentre = new simple_html_dom();
+    $domCentre->load($htmlCentre);
+    //find the announces in the current page and store the records
+    findAnnounces ($domCentre);
+    //look for the next page link
+    $value = "";
+    foreach($domCentre->find('.next') as $data){
+        $value = $data->href;
+    }
+}
+
+/***************** Functions *********************/
+function findAnnounces($strDataDom){
+    //for each page, there are up to 10 records, each of them marked with DOM class 'plus'
+    foreach($strDataDom->find('.plus') as $data){
+        $value = $data->href;
+        //is it a valid URL for an announce?
+        if (strrpos ($value , "www.athome.lu/")){
+            //go into the announce
+            $htmlContent = scraperWiki::scrape($value);
+            //look for the start of the json record wich has all the information of the announce
+            //and manually trim it to the correct json format
+            $strStart = strpos($htmlContent, "initGoogleMap");
+            $strEnd = strpos($htmlContent, "#containerGoogleMap");
+    
+            $strData = substr ($htmlContent, $strStart, $strEnd - $strStart - 6);     
+            $strData = ltrim($strData, "initGoogleMap([");
+            //is it UTF format? just in case we convert it   
+            $strDataUTF = iconv('UTF-8', 'ASCII//TRANSLIT', $strData);
+            //the function will transform the string into a json object and store it in the database
+            storeJson($strDataUTF);
+        }
+    }
+}
+
+/*************************************************/
+function storeJson($strData){
+    $record=array(); 
+    //decode the string
+    $jsonVar = json_decode($strData);
+    //if the decode ended with no error
+    if (json_last_error() === JSON_ERROR_NONE) { 
+        $record["id"] = $jsonVar -> id;
+        $record["submitter"] = $jsonVar -> submitter;
+        $record["inserted"] = $jsonVar -> inserted;
+        $record["immotype"] = $jsonVar -> immotype;
+        $record["price"] = $jsonVar -> price;
+	$record["rent"] = $jsonVar -> rent;
+        $record["location"] = $jsonVar -> location;
+        $record["surface"] = $jsonVar -> surface;
+        $record["city"] = $jsonVar -> city;
+        $record["price_by_m2"] = $jsonVar -> price_by_m2;
+        
+        //save the record
+        if ($record["price"] <> 0 or $record["rent"] <> 0 or $record["price_by_m2"] <> 0) {
+           scraperwiki::save_sqlite(array('id'), $record);
+        }
+    }    
+} 
+
+?>
